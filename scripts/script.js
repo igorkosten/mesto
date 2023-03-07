119f5fd22b85cb1ee865f715817db05b60a9b5c1
igorkosten <igorkosten@yandex.ru>
2023-03-07 18:13:21 +0300

    Промежуточная фиксация 5.

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,31 @@
-const form = document.querySelector('.form')
-const InputName = document.querySelector('.input__name')
-const ProfileTitle = document.querySelector('.profile__title')
-const FormButtonClose = document.querySelector('.form__button_close')
-const ProfileEditButton = document.querySelector('.profile__edit-button')
-const Popup = document.querySelector('.popup')
-
+const form = document.querySelector('.popup__form');
+const inputName = document.querySelector('.input__name');
+const inputDesciptoin = document.querySelector('.input__desciptoin')
+const profileTitle = document.querySelector('.profile__title')
+const profileSubTitle = document.querySelector('.profile__subtitle')
+const formButtonClose = document.querySelector('.popup__form-button-close')
+const profileEditButton = document.querySelector('.profile__edit-button')
+const popup = document.querySelector('.popup')
 
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log(InputName.value)
-  ProfileTitle.innerHTML=(InputName.value)
+
+  profileTitle.innerHTML = inputName.value;
+  inputName.value = '';
+
+  profileSubTitle.innerHTML = inputDesciptoin.value;
+  inputDesciptoin.value = '';
+
+  popup.classList.remove('active')
 })
 
-FormButtonClose.onClick=function(){
-  Popup.classList.remove('active')
-}
 
-ProfileEditButton.onClick=function(){
-  Popup.classList.add('active')
-}
+formButtonClose.addEventListener('click', function (){
+  popup.classList.remove('active')
+})
+
+profileEditButton.addEventListener('click', function (){
+  popup.classList.add('active')
+})
+
---
+++ scripts/script_like.js
@@ -0,0 +1,7 @@
+const likeButton = document.querySelector('.elements__like-button')
+
+
+likeButton.onclick=function (){
+  likeButton.classList.add('activ')
+}
+
