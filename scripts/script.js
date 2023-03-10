e2bb94c500217664570aea946c37a95fd9de3d45
igorkosten <igorkosten@yandex.ru>
2023-03-10 17:26:47 +0300

    Исправлены замечания ревьюера.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-const form = document.querySelector('.popup__form');
-const inputName = document.querySelector('.popup__form-input-name');
-const inputDesciptoin = document.querySelector('.popup__form-input-desciptoin')
+const container = document.querySelector('.popup__container');
+const inputName = document.querySelector('.popup__input-name');
+const inputDesciptoin = document.querySelector('.popup__input-desciptoin')
 const profileTitle = document.querySelector('.profile__title')
 const profileSubTitle = document.querySelector('.profile__subtitle')
-const formButtonClose = document.querySelector('.popup__form-button-close')
+const ButtonClose = document.querySelector('.popup__button-close')
 const profileEditButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const likeButton = document.querySelectorAll('.elements__like-button')
 
-form.addEventListener('submit', (event) => {
+container.addEventListener('submit', (event) => {
   event.preventDefault();
 
   profileTitle.innerHTML = inputName.value;
@@ -17,35 +17,27 @@ form.addEventListener('submit', (event) => {
   profileSubTitle.innerHTML = inputDesciptoin.value;
   inputDesciptoin.value = '';
 
-  popup.classList.remove('active')
+  popup.classList.remove('popup_opened')
 })
 
-
-formButtonClose.addEventListener('click', function (){
-  popup.classList.remove('active')
+ButtonClose.addEventListener('click', function (){
+  popup.classList.remove('popup_opened')
 })
 
 profileEditButton.addEventListener('click', function (){
   inputName.value = profileTitle.innerHTML;
   inputDesciptoin.value = profileSubTitle.innerHTML;
-  popup.classList.add('active')
+  popup.classList.add('popup_opened')
 })
-
-// profileEditButton.addEventListener('click', function (){
-
-// inputName.innerHTML = profileTitle.value;
-// profileTitle.value = '';
-// })
-
 
 likeButton.forEach(function (element) {
   element.onclick=function (){
-    if (element.classList.contains('activ'))
+    if (element.classList.contains('elements__like-button_active'))
   {
-    element.classList.remove('activ')
+    element.classList.remove('elements__like-button_active')
   }
     else {
-      element.classList.add('activ')
+      element.classList.add('elements__like-button_active')
     }
   }
 });
