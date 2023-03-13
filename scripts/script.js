8dc9e9e4c4f11691ccd859a86d9ea1e434a2f6a4
igorkosten <igorkosten@yandex.ru>
2023-03-13 20:03:50 +0300

    Исправлены замечания ревьюера.

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,41 @@
 const container = document.querySelector('.popup__container');
-const inputName = document.querySelector('.popup__input-name');
-const inputDesciptoin = document.querySelector('.popup__input-desciptoin')
+const inputName = document.querySelector('.popup__input_name');
+const inputDesciptoin = document.querySelector('.popup__input_desciptoin')
 const profileTitle = document.querySelector('.profile__title')
 const profileSubTitle = document.querySelector('.profile__subtitle')
-const ButtonClose = document.querySelector('.popup__button-close')
+const buttonClose = document.querySelector('.popup__button-close')
 const profileEditButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
-const likeButton = document.querySelectorAll('.elements__like-button')
+// const likeButton = document.querySelectorAll('.elements__like-button')
+
+function remove(){
+  popup.classList.remove('popup_opened')
+}
 
 container.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  profileTitle.innerHTML = inputName.value;
-  inputName.value = '';
-
-  profileSubTitle.innerHTML = inputDesciptoin.value;
-  inputDesciptoin.value = '';
-
-  popup.classList.remove('popup_opened')
+  profileTitle.textContent = inputName.value;
+  profileSubTitle.textContent = inputDesciptoin.value;
+  remove()
 })
 
-ButtonClose.addEventListener('click', function (){
-  popup.classList.remove('popup_opened')
-})
+buttonClose.addEventListener('click', remove
+)
 
 profileEditButton.addEventListener('click', function (){
-  inputName.value = profileTitle.innerHTML;
-  inputDesciptoin.value = profileSubTitle.innerHTML;
+  inputName.value = profileTitle.textContent;
+  inputDesciptoin.value = profileSubTitle.textContent;
   popup.classList.add('popup_opened')
 })
 
-likeButton.forEach(function (element) {
-  element.onclick=function (){
-    if (element.classList.contains('elements__like-button_active'))
-  {
-    element.classList.remove('elements__like-button_active')
-  }
-    else {
-      element.classList.add('elements__like-button_active')
-    }
-  }
-});
+// likeButton.forEach(function (element) {
+//   element.onclick=function (){
+//     if (element.classList.contains('elements__like-button_active'))
+//   {
+//     element.classList.remove('elements__like-button_active')
+//   }
+//     else {
+//       element.classList.add('elements__like-button_active')
+//     }
+//   }
+// });
